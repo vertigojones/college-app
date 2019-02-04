3bdab9916a42af95549207e63c9b232092042120
owenliversidge <owen.liversidge@gmail.com>
2019-02-04 11:25:27 -0500

    some code refactoring

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,10 +1,12 @@
 class StudentsController < ApplicationController
+    before_action :set_student, only: [:show, :edit, :update]
+
     def index
         @students = Student.all
     end
 
     def show
-        @student = Student.find(params[:id])
+
     end
 
     def new
@@ -15,18 +17,17 @@ class StudentsController < ApplicationController
         @student = Student.create(student_params)
         if @student.save
             flash[:notice] = "You have successfully gigned up!"
-            redirect_to root_path
+            redirect_to @student
         else
             render "new"
         end
     end
 
     def edit
-        @student = Student.find(params[:id])
+       
     end
 
     def update
-        @student = Student.find(params[:id])
         if @student.update(student_params)
             flash[:notice] = "You have successfully updated your profile!"
             redirect_to @student
@@ -36,6 +37,10 @@ class StudentsController < ApplicationController
     end
 
     private 
+
+    def set_student
+        @student = Student.find(params[:id])
+    end
 
     def student_params
         params.require(:student).permit(:name, :email)
