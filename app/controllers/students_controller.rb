84b885f19999c2c5557c294400c1feafa313bc74
owenliversidge <owen.liversidge@gmail.com>
2019-02-04 09:28:43 -0500

    successfully added students model with new student working and errors

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,25 @@
+class StudentsController < ApplicationController
+    def index
+        @students = Student.all
+    end
+
+    def new
+        @student = Student.new
+    end
+
+    def create
+        @student = Student.create(student_params)
+        if @student.save
+            flash[:success] = "You have successfully gigned up!"
+            redirect_to root_path
+        else
+            render "new"
+        end
+    end
+
+    private 
+
+    def student_params
+        params.require(:student).permit(:name, :email)
+    end
+end
