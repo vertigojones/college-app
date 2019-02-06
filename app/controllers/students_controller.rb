dfd6d2e859733448ec900fe09f9ac75bb9a504b0
owenliversidge <owen.liversidge@gmail.com>
2019-02-06 09:38:54 -0500

    edit functionality now only available to current user

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,7 @@
 class StudentsController < ApplicationController
+    skip_before_action :require_user, only: [:new, :create]
     before_action :set_student, only: [:show, :edit, :update]
+    before_action :require_same_student, only: [:edit, :update]
 
     def index
         @students = Student.all
@@ -44,5 +46,12 @@ class StudentsController < ApplicationController
 
     def student_params
         params.require(:student).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def require_same_student
+        if current_user != @student
+            redirect_to student_path(current_user)
+            flash[:notice] = "You can only edit your own profile"
+        end
     end
 end
